7665242f6b25a412aa80b8af2cf0509c086e7db9
jungminlee <iis05251@sookmyung.ac.kr>
2022-08-11 00:57:29 +0900

    지도 기능 추가

--- frontend/src/screens/Enterprise.js
+++ frontend/src/screens/Enterprise.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { TouchableWithoutFeedback,Keyboard,ImageBackground, FlatList  } from 'react-native';
 import styled from 'styled-components/native';
 import { Button } from '../components';
@@ -7,13 +7,12 @@ import { Image } from 'react-native';
 import { StyleSheet, Text,View } from 'react-native';
 import { ScrollView, TouchableOpacity } from 'react-native-gesture-handler';
 import { Dimensions } from 'react-native';
-import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
-import SwiperView from 'react-native-swiper-view';
 import { Linking } from 'react-native';
+import axios from 'axios';
+import MapView, {Marker} from 'react-native-maps';
 
 const { width } = Dimensions.get('window');
 
-const Kakao_map='kakaomap://place?id=7813433'
 
 const Container = styled.View`
     flex : 1;
@@ -71,6 +70,7 @@ text5:{
     fontSize: 15,
   }
 }
+const searching='중문 해수욕장'
 
 const cost='1시간 25,000 \n 2시간 40,000 \n 3시간 50,000';
 const data='SA 국제서핑연맹 자격증 보유, KSL프로서퍼 강사와 함께하는 체계적이고 안전한 서핑커리큘럼 \n 내용 줄바꿈 확인용 \n 추가 되는 내용은  \n 스크롤뷰로 확인하려고 함 \n 몇줄까지 보여도 되는 건지?'
@@ -102,11 +102,68 @@ const state =[
 
 const Enterprise = () => {
 
+    let query='https://dapi.kakao.com/v2/local/search/keyword.json?query={}'
+    query=query.replace('{}',searching)
+    const [pid, setPid] = useState('');
+    const [px,setPx]=useState(0);
+    const [py,setPy]=useState(0);
+    const callApi = async ()=> {
+        try{
+            setPid(null);
+            const res=await axios.get (
+                    query,
+                    {
+                        headers:{
+                            Authorization:"KakaoAK 62e37fc1387a408315981029aef3f771",
+                        },
+                    },
+                );
+                const location=res.data.documents[0];
+                const p_id=location.id
+                const p_x=location.x
+                const p_y=location.y
+                setPid(p_id)
+                setPx(p_x)
+                setPy(p_y)
+        }catch(error){
+            console.log(error.message);
+        }
+    };
+
+    useEffect(()=>{
+        callApi();
+    },[]);
+
+    let p_id=String(pid)
+    let Kakao_map='kakaomap://place?id={}'
+    Kakao_map=Kakao_map.replace('{}',p_id)
+    let p_x=Number(px)
+    let p_y=Number(py)
+    let x=Number(p_x.toFixed(4))
+    let y=Number(p_y.toFixed(4))
+ 
+    const [num, setNum] = useState(0);
+  
+    const onIncrease = () => {
+      setNum(num + 1);
+    }
+    
+    if (num%2===0) {
+      iconname='bookmark-outline';
+    } else {
+      iconname='bookmark';
+      alert('북마크에 저장되었습니다.');
+    }
+
+
     return (
         <ScrollView>
             <Container>
             <View style={{flexDirection: 'row', flex:0.5, alignItems:'flex-start'}}>
-                    <Text style={{fontSize:20, margin:10, marginTop:30, marginRight:width/2-20}}>중문 해수욕장</Text>
+                    <Text style={{fontSize:20, margin:10, marginTop:30, marginRight:width/2-20}}>{searching}</Text>
+                    <TouchableOpacity onPress={onIncrease} >
+                    <Icon name={iconname} size={25}  color='red' style={{margin:10, marginTop:30}}/>
+                    </TouchableOpacity>
                 </View>
             <View style={{flexDirection: 'row', flex:0.5, alignItems:'flex-start'}}>
                 <Text style={styles.text1}> 위치 정보 </Text>
@@ -114,7 +171,21 @@ const Enterprise = () => {
                     <Text style={{marginLeft:width-250, marginTop:10, marginBottom:10, backgroundColor:'#b8b4ad',color:'#ffffff',fontSize:17}}>카카오맵에서 보기</Text>
                 </TouchableOpacity>
             </View>
-            <Image style={styles.image} source={require('../images/spot_1_ocean.jpg')} />
+            <View>
+            <MapView 
+                style={{width: Dimensions.get('window').width-30,height: 250}}
+                initialRegion={{
+                    latitude: y,
+                    longitude: x,
+                    latitudeDelta: 0.0922,
+                    longitudeDelta: 0.0421,
+                  }}
+                >
+                <Marker
+                    coordinate={{latitude: y, longitude: x}}
+                    />
+                </MapView>
+            </View>
             <View style={{flexDirection: 'row', flex:1, justifyContent:'space-around', paddingTop:15}}>
                 <Text style={styles.text1}>가격 정보</Text>
                 <View style={{paddingLeft:width-450,paddingRight:width/2-20}}>
