1e74c194c6c0742ff12b8b56735a25e029090af5
by2lucy <hjeongzz@naver.com>
2022-08-29 13:45:33 +0900

    [b] bookmark, heart models.py

--- backend/saranghae/community/models.py
+++ backend/saranghae/community/models.py
@@ -1,18 +1,29 @@
 from django.db import models
 from django.utils import timezone 
+from user.models import User
 
 class Magazine(models.Model):
-    # author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="boards????")
-    writer = models.CharField(max_length=128, null=False)
+    writer = models.CharField(max_length=128, null=False) # superuser
     title = models.CharField(max_length=128, null=False)
     overview = models.TextField()
     content = models.TextField()
-    # thumbnail = models.ImageField(blank=True, upload_to="images", null=True)
+    thumbnail = models.ImageField(upload_to="media/mz", blank=True, null=True)
     create_at = models.DateTimeField("create_at", auto_now_add=True)
     # tags = models.ManyToManyField(Tag, verbose_name='태그', blank=True)
 
     def __str__(self):
         return self.title
+
+class Heart(models.Model):
+    user_id = models.ForeignKey('user.User', on_delete=models.CASCADE, related_name="heart")
+    contentid = models.CharField(max_length=128, null=False)
+    contenttypeid = models.CharField(max_length=128, null=False)
+    likeYn = models.BooleanField(default=False)
+
+class Bookmark(models.Model):
+    user_id = models.ForeignKey('user.User', on_delete=models.CASCADE, related_name="bookmark")
+    mz_id = models.ForeignKey('Magazine', on_delete=models.CASCADE, related_name="bookmark")
+    bookmarkYn = models.BooleanField(default=False)
 
 
 # class Magazine_Comment(models.Model):
